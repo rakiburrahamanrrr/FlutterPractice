0a8fc52a5787514d5666169765a4cb8fdae2c877
Rakibur rahman <rakibur.rahaman007@outlook.com>
2022-04-24 16:34:27 +0600

    name_changed

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter App'),
+      home: const MyHomePage(title: 'Flutter Appp'),
     );
   }
 }
